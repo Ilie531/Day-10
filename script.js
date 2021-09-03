5ed8cbe80f397f0dd27ee8e0d0c814dfd877f7bd
Ilie531 <trifanilie2002@gmail.com>
2021-09-03 10:04:28 +0200

    Finished first exercise
    Finished first exercise with json, didn't even use .json this some bs.

---
+++ script.js
@@ -0,0 +1,11 @@
+/*window.onload; {
+    const clients = '{"firstName":["Joe", "Bo", "George"], "lastName":["Biden", "Burnham", "Carlin"]}';
+const myObj = JSON.parse(clients);
+var text="";
+for (let i = 0; i < myObj.firstName.length; i++)
+text += myObj.firstName[i] + " " + myObj.lastName[i] +"<br>";
+console.log(text);
+document.getElementById('test').innerHTML = text;
+
+}*/ //Exercise complete, but why the fuck would I ever use JSON, someone explain this shit; it isn't worth the hassle.
+//also fuck .onload, just throw your script at the end of the html so you know it'll activate.
